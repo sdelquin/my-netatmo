b8c05d93fed2a770c2f1dedfff1ec1d58f963a9d
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-01-28 07:35:29 +0000

    Add checkings when getting values from API

--- my_netatmo/services.py
+++ my_netatmo/services.py
@@ -13,9 +13,9 @@ def get_measures(device: str):
     ws.get_data()
     response = ws.station_by_name()
     if device == 'INDOOR':
-        data = response['dashboard_data']
+        data = response.get('dashboard_data', 'NA')
     elif device == 'OUTDOOR':
-        data = response['modules'][0]['dashboard_data']
+        data = response['modules'][0].get('dashboard_data', 'NA')
     return data
 
 
@@ -28,8 +28,8 @@ def timestamp_to_hour(timestamp: int):
 
 
 def display_leader_measures(outdoor_data: dict, indoor_data: dict, menu: xbarmenu.Menu):
-    outdoor_temp = outdoor_data['Temperature']
-    indoor_temp = indoor_data['Temperature']
+    outdoor_temp = outdoor_data.get('Temperature', 'NA')
+    indoor_temp = indoor_data.get('Temperature', 'NA')
     temp_diff = 'up' if outdoor_temp - indoor_temp > 0 else 'down'
     temp_diff = MEASURES_TRENDS.get(temp_diff, '')
     temp_display = f'{outdoor_temp}º {temp_diff}'.strip()
@@ -37,61 +37,81 @@ def display_leader_measures(outdoor_data: dict, indoor_data: dict, menu: xbarmen
 
     menu.add_sep()
 
-    last_update = timestamp_to_hour(outdoor_data['time_utc'])
-    last_update_display = f'Last update: {last_update}h'
+    try:
+        last_update = timestamp_to_hour(outdoor_data['time_utc'])
+        last_update += 'h'
+    except KeyError:
+        last_update = 'NA'
+    last_update_display = f'Last update: {last_update}'
     menu.add_item(last_update_display)
 
 
 def display_outdoor_measures(data: dict, menu: xbarmenu.Menu):
-    temp = data['Temperature']
-    temp_trend = MEASURES_TRENDS.get(data['temp_trend'], '')
+    temp = data.get('Temperature', 'NA')
+    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', 'NA'), '')
     temp_display = f'{temp}º {temp_trend}'.strip()
     menu.add_item(temp_display)
 
-    humidity = data['Humidity']
+    humidity = data.get('Humidity', 'NA')
     humidity_display = f'H: {humidity}%'
     menu.add_item(humidity_display)
 
-    when_min_temp = timestamp_to_hour(data['date_min_temp'])
-    min_temp = data['min_temp']
-    min_temp_display = f'↓ {min_temp}º ({when_min_temp}h)'
+    try:
+        when_min_temp = timestamp_to_hour(data['date_min_temp'])
+        when_min_temp += 'h'
+    except KeyError:
+        when_min_temp = 'NA'
+    min_temp = data.get('min_temp', 'NA')
+    min_temp_display = f'↓ {min_temp}º ({when_min_temp})'
     menu.add_item(min_temp_display)
 
-    when_max_temp = timestamp_to_hour(data['date_max_temp'])
-    max_temp = data['max_temp']
-    max_temp_display = f'↑ {max_temp}º ({when_max_temp}h)'
+    try:
+        when_max_temp = timestamp_to_hour(data['date_max_temp'])
+        when_max_temp += 'h'
+    except KeyError:
+        when_max_temp = 'NA'
+    max_temp = data.get('max_temp', 'NA')
+    max_temp_display = f'↑ {max_temp}º ({when_max_temp})'
     menu.add_item(max_temp_display)
 
 
 def display_indoor_measures(data: dict, menu: xbarmenu.Menu):
-    temp = data['Temperature']
-    temp_trend = MEASURES_TRENDS.get(data['temp_trend'], '')
+    temp = data.get('Temperature', 'NA')
+    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', 'NA'), '')
     temp_display = f'{temp}º {temp_trend}'.strip()
     menu.add_item(temp_display)
 
-    humidity = data['Humidity']
+    humidity = data.get('Humidity', 'NA')
     humidity_display = f'H: {humidity}%'
     menu.add_item(humidity_display)
 
-    co2 = data['CO2']
+    co2 = data.get('CO2', 'NA')
     co2_display = f'CO₂: {co2}ppm'
     menu.add_item(co2_display)
 
-    pressure = data['Pressure']
-    pressure_trend = MEASURES_TRENDS.get(data['pressure_trend'], '')
+    pressure = data.get('Pressure', 'NA')
+    pressure_trend = MEASURES_TRENDS.get(data.get('pressure_trend', 'NA'), '')
     pressure_display = f'P: {pressure}mbar {pressure_trend}'.strip()
     menu.add_item(pressure_display)
 
-    noise = data['Noise']
+    noise = data.get('Noise', 'NA')
     noise_display = f'N: {noise}dB'
     menu.add_item(noise_display)
 
-    when_min_temp = timestamp_to_hour(data['date_min_temp'])
-    min_temp = data['min_temp']
+    try:
+        when_min_temp = timestamp_to_hour(data['date_min_temp'])
+        when_min_temp += 'h'
+    except KeyError:
+        when_min_temp = 'NA'
+    min_temp = data.get('min_temp', 'NA')
     min_temp_display = f'↓ {min_temp}º ({when_min_temp}h)'
     menu.add_item(min_temp_display)
 
-    when_max_temp = timestamp_to_hour(data['date_max_temp'])
-    max_temp = data['max_temp']
+    try:
+        when_max_temp = timestamp_to_hour(data['date_max_temp'])
+        when_max_temp += 'h'
+    except KeyError:
+        when_max_temp = 'NA'
+    max_temp = data.get('max_temp', 'NA')
     max_temp_display = f'↑ {max_temp}º ({when_max_temp}h)'
     menu.add_item(max_temp_display)
