7cdfd3ae582caf040fb99a6a0917e39d9f3ab5db
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-01-28 07:39:59 +0000

    Add constant for NA values

--- my_netatmo/services.py
+++ my_netatmo/services.py
@@ -6,6 +6,7 @@ import netatmo
 from my_netatmo import xbarmenu
 
 MEASURES_TRENDS = {'down': '↓', 'up': '↑'}
+NA_VALUE = 'NA'  # value for non-available measurements
 
 
 def get_measures(device: str):
@@ -13,9 +14,9 @@ def get_measures(device: str):
     ws.get_data()
     response = ws.station_by_name()
     if device == 'INDOOR':
-        data = response.get('dashboard_data', 'NA')
+        data = response.get('dashboard_data', NA_VALUE)
     elif device == 'OUTDOOR':
-        data = response['modules'][0].get('dashboard_data', 'NA')
+        data = response['modules'][0].get('dashboard_data', NA_VALUE)
     return data
 
 
@@ -28,8 +29,8 @@ def timestamp_to_hour(timestamp: int):
 
 
 def display_leader_measures(outdoor_data: dict, indoor_data: dict, menu: xbarmenu.Menu):
-    outdoor_temp = outdoor_data.get('Temperature', 'NA')
-    indoor_temp = indoor_data.get('Temperature', 'NA')
+    outdoor_temp = outdoor_data.get('Temperature', NA_VALUE)
+    indoor_temp = indoor_data.get('Temperature', NA_VALUE)
     temp_diff = 'up' if outdoor_temp - indoor_temp > 0 else 'down'
     temp_diff = MEASURES_TRENDS.get(temp_diff, '')
     temp_display = f'{outdoor_temp}º {temp_diff}'.strip()
@@ -41,18 +42,18 @@ def display_leader_measures(outdoor_data: dict, indoor_data: dict, menu: xbarmen
         last_update = timestamp_to_hour(outdoor_data['time_utc'])
         last_update += 'h'
     except KeyError:
-        last_update = 'NA'
+        last_update = NA_VALUE
     last_update_display = f'Last update: {last_update}'
     menu.add_item(last_update_display)
 
 
 def display_outdoor_measures(data: dict, menu: xbarmenu.Menu):
-    temp = data.get('Temperature', 'NA')
-    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', 'NA'), '')
+    temp = data.get('Temperature', NA_VALUE)
+    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', NA_VALUE), '')
     temp_display = f'{temp}º {temp_trend}'.strip()
     menu.add_item(temp_display)
 
-    humidity = data.get('Humidity', 'NA')
+    humidity = data.get('Humidity', NA_VALUE)
     humidity_display = f'H: {humidity}%'
     menu.add_item(humidity_display)
 
@@ -60,8 +61,8 @@ def display_outdoor_measures(data: dict, menu: xbarmenu.Menu):
         when_min_temp = timestamp_to_hour(data['date_min_temp'])
         when_min_temp += 'h'
     except KeyError:
-        when_min_temp = 'NA'
-    min_temp = data.get('min_temp', 'NA')
+        when_min_temp = NA_VALUE
+    min_temp = data.get('min_temp', NA_VALUE)
     min_temp_display = f'↓ {min_temp}º ({when_min_temp})'
     menu.add_item(min_temp_display)
 
@@ -69,32 +70,32 @@ def display_outdoor_measures(data: dict, menu: xbarmenu.Menu):
         when_max_temp = timestamp_to_hour(data['date_max_temp'])
         when_max_temp += 'h'
     except KeyError:
-        when_max_temp = 'NA'
-    max_temp = data.get('max_temp', 'NA')
+        when_max_temp = NA_VALUE
+    max_temp = data.get('max_temp', NA_VALUE)
     max_temp_display = f'↑ {max_temp}º ({when_max_temp})'
     menu.add_item(max_temp_display)
 
 
 def display_indoor_measures(data: dict, menu: xbarmenu.Menu):
-    temp = data.get('Temperature', 'NA')
-    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', 'NA'), '')
+    temp = data.get('Temperature', NA_VALUE)
+    temp_trend = MEASURES_TRENDS.get(data.get('temp_trend', NA_VALUE), '')
     temp_display = f'{temp}º {temp_trend}'.strip()
     menu.add_item(temp_display)
 
-    humidity = data.get('Humidity', 'NA')
+    humidity = data.get('Humidity', NA_VALUE)
     humidity_display = f'H: {humidity}%'
     menu.add_item(humidity_display)
 
-    co2 = data.get('CO2', 'NA')
+    co2 = data.get('CO2', NA_VALUE)
     co2_display = f'CO₂: {co2}ppm'
     menu.add_item(co2_display)
 
-    pressure = data.get('Pressure', 'NA')
-    pressure_trend = MEASURES_TRENDS.get(data.get('pressure_trend', 'NA'), '')
+    pressure = data.get('Pressure', NA_VALUE)
+    pressure_trend = MEASURES_TRENDS.get(data.get('pressure_trend', NA_VALUE), '')
     pressure_display = f'P: {pressure}mbar {pressure_trend}'.strip()
     menu.add_item(pressure_display)
 
-    noise = data.get('Noise', 'NA')
+    noise = data.get('Noise', NA_VALUE)
     noise_display = f'N: {noise}dB'
     menu.add_item(noise_display)
 
@@ -102,8 +103,8 @@ def display_indoor_measures(data: dict, menu: xbarmenu.Menu):
         when_min_temp = timestamp_to_hour(data['date_min_temp'])
         when_min_temp += 'h'
     except KeyError:
-        when_min_temp = 'NA'
-    min_temp = data.get('min_temp', 'NA')
+        when_min_temp = NA_VALUE
+    min_temp = data.get('min_temp', NA_VALUE)
     min_temp_display = f'↓ {min_temp}º ({when_min_temp}h)'
     menu.add_item(min_temp_display)
 
@@ -111,7 +112,7 @@ def display_indoor_measures(data: dict, menu: xbarmenu.Menu):
         when_max_temp = timestamp_to_hour(data['date_max_temp'])
         when_max_temp += 'h'
     except KeyError:
-        when_max_temp = 'NA'
-    max_temp = data.get('max_temp', 'NA')
+        when_max_temp = NA_VALUE
+    max_temp = data.get('max_temp', NA_VALUE)
     max_temp_display = f'↑ {max_temp}º ({when_max_temp}h)'
     menu.add_item(max_temp_display)
