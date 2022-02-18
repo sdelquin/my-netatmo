8b1b98a3108239641e9e7f19df57d57a822a7165
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-02-18 07:11:32 +0000

    Check connection errors

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import requests
+
 from my_netatmo import services, xbarmenu
 
 menu = xbarmenu.Menu()
-outdoor_data = services.get_outdoor_measures()
-indoor_data = services.get_indoor_measures()
+try:
+    outdoor_data = services.get_outdoor_measures()
+    indoor_data = services.get_indoor_measures()
+except requests.exceptions.ConnectionError:
+    menu.add_item('⚠️ NA')
+else:
+    services.display_leader_measures(outdoor_data, indoor_data, menu)
 
-services.display_leader_measures(outdoor_data, indoor_data, menu)
+    outdoor_menu = menu.add_item('☁️ Outdoor')
+    services.display_outdoor_measures(outdoor_data, outdoor_menu)
 
-outdoor_menu = menu.add_item('☁️ Outdoor')
-services.display_outdoor_measures(outdoor_data, outdoor_menu)
+    indoor_menu = menu.add_item('🏠 Indoor')
+    services.display_indoor_measures(indoor_data, indoor_menu)
 
-indoor_menu = menu.add_item('🏠 Indoor')
-services.display_indoor_measures(indoor_data, indoor_menu)
-
-menu.add_item('🌐 Netatmo dashboard', href='https://my.netatmo.com/app/station')
+    menu.add_item('🌐 Netatmo dashboard', href='https://my.netatmo.com/app/station')
 
 print(menu)
